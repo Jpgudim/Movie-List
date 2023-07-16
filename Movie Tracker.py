25650f524b2f1a1f55ce8fbd620f667cbd431832
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-07-15 23:38:48 -0500

    Big changes
    The program now takes the users input and adds it to a text file instead of a list.

--- Movie Tracker.py
+++ Movie Tracker.py
@@ -1,3 +1,5 @@
+list_file = open("Movie_List.txt", "a")
+
 print("Did you watch a movie? y/n")
 check = input("")
 if check != "y":
@@ -6,15 +8,20 @@ if check != "y":
 else:
     print ("What movie did you watch?")
     movie = input("")
-movie_list = []
-movie_list.append(movie)
+    movie = str(movie)
+list_file.write(movie + "\n")
 print()
 print ("Got it! That movie is added to the list")
 print()
+list_file.close()
+list_file = open("Movie_list.txt", "r")
 print ("Would you like to see your list? y/n")
 check2 = input("")
 if check2 != "y":
     print ("Ok. Goodbye!")
     print ("program terminated")
 else:
-    print ("your list: " + str(movie_list))
+    movie_list = list_file.read()
+    print()
+    print (movie_list)
+list_file.close()
