438e87953fe9c50b854db3c5f9e8e4a3b480b25d
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-07-17 23:53:38 -0500

    Working on loops for multiple inputs
    - Created functions for asking the user what movie they watched and if they want to print the contents of the text file.
    
    - Created a loop to allow the user to enter multiple movies
    
    - would like to add a loop in the future that asks the user if they want to print the contents of the file after each movie is entered.

--- Movie Tracker.py
+++ Movie Tracker.py
@@ -1,24 +1,39 @@
 import datetime
 
-list_file = open("Movie_List.txt", "a")
+list_file = open("Movie_List.txt", "a+")
 
-print ("What movie did you watch?")
-movie = input("")
-movie = str(movie)
-date = datetime.datetime.now()
-list_file.write(movie + "\n" + " - " + date.strftime("%x") + "\n")
-print()
-print ("Got it! That movie is added to the list")
-print()
-list_file.close()
-list_file = open("Movie_list.txt", "r")
-print ("Would you like to see your list? y/n")
-check2 = input("")
-if check2 != "y":
-    print ("Ok. Goodbye!")
-    print ("program terminated")
-else:
-    movie_list = list_file.read()
+def get_movie():
+    print ("What movie did you watch?")
+    movie = input("")
+    movie = str(movie)
+    date = datetime.datetime.now()
+    list_file.write("\n" + movie + "\n" + " - " + date.strftime("%x") + "\n")
+    ()
+    print ("Got it! That movie is added to the list")
     print()
-    print (movie_list)
+
+get_movie()
+
+def print_list():
+    movie_list = list_file.read()
+    print ("Would you like to see your list? y/n")
+    check = input("")
+    if check == "y":
+        print (movie_list)
+    return check
+
+check = print_list()
+while check == "n":
+    print ("would you like to add another movie? y/n")
+    check2 = input("")
+    if check2 == "y":
+        get_movie()
+    if check2 != "y":
+        print ("Ok. Goodbye!")
+        print ("program terminated. File Closed")
+        check = ""
+
+
+
+
 list_file.close()
